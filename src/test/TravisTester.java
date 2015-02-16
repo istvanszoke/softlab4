2f78a428fd81ced0a2ca116a95318654e3bca762
Nyári Dávid Tamás <nyaridavid@gmail.com>
2015-02-16 18:01:57 +0100

    Restore Travis test

--- src/test/TravisTester.java
+++ src/test/TravisTester.java
@@ -1,13 +1,5 @@
-package test;
-
 import org.junit.Test;
 import static org.junit.Assert.*;
-
-/**
- * This is a proof of concept test class.
- * For some reason it has never failed before.
- * And if out genius is truly great it never will.
- */
 public class TravisTester {
 
     @Test
