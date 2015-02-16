ab404232c8c9d5b4ca9098e3c424467f4107ea24
Nyári Dávid Tamás <nyaridavid@gmail.com>
2015-02-16 16:04:04 +0100

    It works somewhat now

--- build_docs.sh
+++ build_docs.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
 echo "Generating of the documentation started."
+
+texdoclet\javadoc_to_latex.sh
+if [ $? -ne 0]; then
+  echo 'Generation of LaTeX JavaDoc failed. [FAILED]'
+  exit -1
+fi
+
 cd docs
 pdflatex -halt-on-error szoftlab4.tex
 if [ $? -ne 0 ]; then
@@ -8,10 +15,5 @@ if [ $? -ne 0 ]; then
   exit -1
 fi
 cd ..
-texdoclet\javadoc_to_latex.sh
-if [ $? -ne 0]; then
-  echo 'Generation of LaTeX JavaDoc failed. [FAILED]'
-  exit -1
-fi
 echo "Generating of the documentation ended. [SUCCESS]"
 exit 0
--- texdoclet/javadoc_to_latex.sh
+++ texdoclet/javadoc_to_latex.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
 
-javavadoc	-docletpath path/to/TeXDoclet.jar \
-    		-doclet org.stfm.texdoclet.TeXDoclet \
-    		-noindex \
-    		-tree \
-    		-hyperref \
-		-include \
-    		-output ../docs/javadoc/javadoc.tex \
-		-sourcepath ../src \
-		-subpackages org:com
+rm ../docs/javadoc/*.tex
+
+OLDDIR=$(pwd)
+cd ../src
+
+javadoc -docletpath ../texdoclet/TeXDoclet.jar \
+	-doclet org.stfm.texdoclet.TeXDoclet \
+	-tree \
+	-noindex \
+	-hyperref \
+	-output ../docs/javadoc/javadoc.tex \
+	-sourcepath . \
+	-subpackages org:main \
+	-include \
+	-sectionlevel section \
+	-serial \
+	-private \
+
+cd $OLDDIR
 
 exit $?	 
