4316494631389deb953084f06410a5fe8c73e782
Paral Zsolt <paralzsolt@gmail.com>
2015-02-20 21:11:29 +0100

    Fixed LaTeX generation. The error is somewhere in the provided LaTeX files, so we're using a workaround for now

--- build_docs.sh
+++ build_docs.sh
@@ -2,18 +2,15 @@
 
 echo "Generating of the documentation started."
 
-for file in docs/*.{aux,lof,log,out,toc};
-do
-    rm -f "$file"
-done
-
 texdoclet/javadoc_to_latex.sh
+
 if [ $? -ne 0 ]; then
   echo 'Generation of LaTeX JavaDoc failed. [FAILED]'
   exit -1
 fi
 
 cd docs
+pdflatex -halt-on-error szoftlab4.tex
 pdflatex -halt-on-error szoftlab4.tex
 if [ $? -ne 0 ]; then
   echo "Generating of the documentation ended. [FAILED]"
@@ -22,3 +19,8 @@ fi
 cd ..
 echo "Generating of the documentation ended. [SUCCESS]"
 exit 0
+
+for file in docs/*.{aux,lof,log,out,toc};
+do
+    rm -f "$file"
+done
