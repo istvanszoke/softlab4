e67c48ce521e86750aed899af6f1045cd814999c
Nyári Dávid Tamás <nyaridavid@gmail.com>
2015-03-17 16:17:37 +0100

    GameCreator osztály kommentelése befejezve

--- src/main/game/GameCreator.java
+++ src/main/game/GameCreator.java
@@ -4,38 +4,85 @@ import java.util.ArrayList;
 
 import field.*;
 
+/**
+ * Játékot generáló osztály
+ * Feladata, hogy beállíthatjuk rajta a játéknak a paramétereit ezzel a játékosok számát
+ * , hogy egy játékos mennyi időt kaphat stb. És ezekből elkészíti a megfelelő játéklogikát
+ */
 public final class GameCreator {
+    /**
+     * A létrehozott Pályának a referenciája
+     */
     private Map map;
+    /**
+     * A Játékosoknak a tömbje
+     */
     private ArrayList<Player> players;
+    /**
+     * Körideje egy játékosnak
+     */
     private int roundTime;
 
+    /**
+     * Játék generáló osztály konstruktora
+     */
     public GameCreator() {
         map = new Map();
         players = new ArrayList<Player>();
         roundTime = -1;
     }
 
+    /**
+     * Körídő beállítása
+     * Játékosok köridejét lehet vele meghatározni másodpercben
+     * @param roundTime - A Játékosoknak szánt köridő másodpercben
+     * @return - Az játék generáló osztály saját referenciája a módosítás után
+     */
     public GameCreator setRoundTime(int roundTime) {
         this.roundTime = roundTime;
         return this;
     }
 
+    /**
+     * Játékos hozzáadása
+     * További játékosokat tudunk hozzáadni a játékhoz
+     * @param player - Az őjonnan hozzáadott játékos referenciája
+     * @return - Az játék generáló osztály saját referenciája a módosítás után
+     */
     public GameCreator addPlayer(Player player) {
         players.add(player);
         return this;
     }
 
+    /**
+     * Meglévő pálya beállítása
+     * Egy már létező pályát beállíthatunk hogy a generált játéklogika majd ezen működjön
+     * @param map - A már létező pálya referenciája
+     * @return - Az játék generáló osztály saját referenciája a módosítás után
+     */
     public GameCreator setMap(Map map) {
         this.map = map;
         return this;
     }
 
+    /**
+     * Új pálya generálása
+     * A játékhoz lehet új pályát generlni ezzel a fügvénnyel a megadott méretben
+     * @param width - A létrehozott pálya szélessége a pálya mezőinek számában
+     * @param height - A létrehozott pálya hosszúsága a pálya mezőinek számában
+     * @return
+     */
     public GameCreator generateTestMap(int width, int height) {
         generateGrid(width, height);
         setNeighbours(width, height);
         return this;
     }
 
+    /**
+     * Játéklogika generálása
+     * Létrehozza a megfelelő játéklogikát, hogy ha nem adtunk meg érvénytelen paramétereket
+     * @return - A kigenerált játék referenciája
+     */
     public Game create() {
         if (map.isEmpty() || players.size() < 2 || players.size() > 4 || roundTime == -1) {
             return null;
@@ -44,6 +91,12 @@ public final class GameCreator {
         return new Game(players, map, roundTime);
     }
 
+    /**
+     * Pálya rács létrehozása
+     * Létrehoz egy megfelelő méretű pályarácsot
+     * @param width - A létrehozott rács szélessége a pálya mezőinek számában
+     * @param height - A létrehozott rács hosszúsága a pálya mezőinek számában
+     */
     private void generateGrid(int width, int height) {
         for (int i = 0; i < height; ++i) {
             for (int j = 0; j < width; ++j) {
@@ -58,6 +111,12 @@ public final class GameCreator {
         }
     }
 
+    /**
+     * Mezők szomszédjainak megadása
+     * Beállítja az összes pályán lévő mezőnek, hogy melyek az ő közvetlen szomszédai
+     * @param width - A pálya szélessége a mezők számában
+     * @param height - A pálya hosszúsága a mezők számában
+     */
     private void setNeighbours(int width, int height) {
         for (int i = 0; i < height; ++i) {
             for (int j = 0; j < width; ++j) {
