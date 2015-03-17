2ca3c876d9039a41d7901d1183cf66c288e6c712
Nyári Dávid Tamás <nyaridavid@gmail.com>
2015-03-17 16:31:18 +0100

    Player osztály kommentelése befejezve

--- src/main/game/Player.java
+++ src/main/game/Player.java
@@ -3,10 +3,25 @@ package game;
 import agents.Agent;
 import agents.Robot;
 
+/**
+ * Egy játékost reprezentáló osztály
+ */
 public class Player {
+    /**
+     * A játékos saját Ágensére egy referencia
+     */
     private Agent agent;
+    /**
+     * A játékos hátralévő ideje milliszekundumban
+     */
     private int timeRemaining;
 
+    /**
+     * Új játékos létrehozása
+     * Létre hoz egy új játékos mely egy Robotot fog irányítani
+     * @param timeInSec - A játékos kezdőideje a másodpercben
+     * @return - A létrehozott játékosnak a Referenciája
+     */
     public static Player createRobot(int timeInSec) {
         Player instance = new Player();
         instance.agent = new Robot();
@@ -14,18 +29,38 @@ public class Player {
         return instance;
     }
 
+    /**
+     * Ágens referenciájának lekérése
+     * Lekérdezhetjük, hogy melyik Ágens irányítását végzi az adott játékos
+     * @return - Az irányított ágensnek a referenciája
+     */
     public Agent getAgent() {
         return agent;
     }
 
+    /**
+     * Hátralévő idő lekérdezése
+     * Lekérdezhetjük a játékostól, hogy még mennyi idő áll a rendelkezésére
+     * @return
+     */
     public int getTimeRemaining() {
         return timeRemaining;
     }
 
+    /**
+     * Hátralévő idő beállítása
+     * Módosíthatjuk az Ágensen, hogy még mennyi ideje maradt a játékban
+     * @param milliseconds - Az új ideje milliszekundumban
+     */
     public void setTimeRemaining(int milliseconds) {
         timeRemaining = milliseconds;
     }
 
+    /**
+     * Lejárt idő lekérdezése
+     * Lekérdezhetjük, hogy az adott játékosnak már elfogyott-e a rendelkezésre álló ideje
+     * @return - Logikai válasz arra, hogy lejárt-e az ideje
+     */
     public boolean isOutOfTime() {
         return timeRemaining <= 0;
     }
