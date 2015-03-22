f02da7e5db4fe28ef2e007baed2c660fb2ffa2b6
Nyári Dávid Tamás <nyaridavid@gmail.com>
2015-03-22 16:01:40 +0100

    Package commentek elkezdése

---
+++ src/main/commands/transmits/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Parncskérések átviteléhez szükséges osztályok csomagja
+ * Feladatuk ezen osztályoknak, hogy köztes utasításátvitelt biztosítsanak
+ * , az ágenseknek irányított kéréseket a mezőnek továbbítsák.
+ */
+package commands.transmits;
