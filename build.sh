fcf462c17f127e8c0a70780ef0ef15d71687673f
Gustorn <gustorn@gmail.com>
2015-03-23 00:28:42 +0100

    Improved file listing script
    To match the file listing adjustements I made in the other branch we now generate file_list.tex in docs/includes/file_list.tex.

--- build.sh
+++ build.sh
@@ -9,15 +9,27 @@ export SRC_DIR="$TOP_DIR/src"
 export LIB_DIR="$TOP_DIR/lib"
 export DOCS_DIR="$TOP_DIR/docs"
 
+source $LIB_DIR/debug_print.sh
+
+latex_file_list() {
+    "$LIB_DIR"/build/latex_file_list.sh > "$DOCS_DIR"/includes/file_list.tex
+    
+    if [ $? -eq 0 ]; then
+        debug_success "LaTeX file list generation successful. [SUCCESS]"
+    fi
+}
+
 if [ "$1" == "docs" ]; then
+    latex_file_list
     "$LIB_DIR"/build/docs.sh "$2"    
 elif [ "$1" == "svg" ]; then
     "$LIB_DIR"/build/svg.sh force
 elif [ "$1" == "latex-file-list" ]; then
-    "$LIB_DIR"/build/latex_file_list.sh
+    latex_file_list
 elif [ "$1" == "java" ]; then
     "$TOP_DIR"/gradlew build
 elif [ "$1" == "all" ]; then
+    latex_file_list
     "$LIB_DIR"/build/svg.sh force
     "$LIB_DIR"/build/docs.sh
     "$TOP_DIR"/gradlew build
