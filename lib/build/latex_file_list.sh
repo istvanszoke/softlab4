b004c49ff6deb2bb677c2eb3472cc25c992b6718
Gustorn <gustorn@gmail.com>
2015-03-22 23:51:26 +0100

    Fixed LaTeX file listing

--- lib/build/latex_file_list.sh
+++ lib/build/latex_file_list.sh
@@ -23,19 +23,28 @@ split_lines() {
     set +f
 }
 
+description=
+get_generic_description() {
+    class="${1%.*}"
+    
+    pre="A"
+    if echo "$class" | grep "^[AaEeIiOoUu].*" > /dev/null; then
+        pre="Az"
+    fi
+
+    description="$pre $class osztály implementációját tartalmazza."
+    if cat "$2" | grep "^.*public interface.*$" > /dev/null; then
+        description="$pre $class interfész deklarációját tartalmazza."
+    fi
+}
+
+get_javadoc_description() {
+    description=$(cat $1 | grep "^ \* .*$" | head -n 1 | cut -c4-)
+}
+
 files=$(find -type f -name *.java -printf '%f;%p;%s\n' | sed s/\\.\\///g | sort -t';' -k2)
 
 split_lines "$files"
-
-longest_path=-1
-for line in "${array[@]}"
-do
-    split_info "$line"
-    
-    if [ "${#path}" -gt "$longest_path" ]; then
-        longest_path="${#path}"
-    fi
-done
 
 echo "\\begin{tabularx}{\linewidth}{| l | l | l | X |}"
 echo "\\hline"
@@ -43,13 +52,22 @@ echo "\\textbf{Fájl neve} & \\textbf{Méret} & \\textbf{Keletkezés ideje} & \\
 echo "\\hline \\hline"
 echo "\\endhead"
 
+
+
 for line in "${array[@]}"
 do
     split_info "$line"
     date=$(git log --diff-filter=A --follow --format=%ai -1 -- "$path" | awk '{gsub(" \\+[0-9]+.*$", ""); gsub(" ", "~"); gsub(":[0-9]+$", "~"); gsub("-", "."); print}')
+    
+    get_javadoc_description "$path"
+
+    if [ -z "$description" ]; then
+        get_generic_description "$name" "$path"
+    fi
 
     echo "\\fajl"
-    printf "{%s}\n{%d byte}\n{%s}\n{...}\n\n" "$path" "$size" "$date"
+    printf "{%s}\n{%d byte}\n{%s}\n{%s}\n\n" "$path" "$size" "$date" "$description"
 done
 
 echo "\\end{tabularx}"
+
