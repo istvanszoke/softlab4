b2ec1e2eace77bd61ceb5e48a1fc2d9c48bf4ed7
Gustorn <gustorn@gmail.com>
2015-03-30 01:41:47 +0200

    Fixed a dumb mistake

--- src/main/game/Heartbeat.java
+++ src/main/game/Heartbeat.java
@@ -64,7 +64,7 @@ public class Heartbeat {
     }
 
     public static void unsubscribe(HeartbeatListener listener) {
-        toRemove.remove(listener);
+        toRemove.add(listener);
     }
 
     public static long getElapsedTime() {
