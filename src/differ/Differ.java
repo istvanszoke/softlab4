7f1b73df85815449808f1f0064d71fc239e439c2
Nyári Dávid Tamás <nyaridavid@gmail.com>
2015-04-19 23:49:03 +0200

    Started differ

---
+++ src/differ/Differ.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+
+/**
+ * Created by nyari on 2015.04.19..
+ */
+public class Differ
+{
+    private BufferedReader left;
+    private BufferedReader right;
+    private boolean correct;
+
+
+    public Differ(BufferedReader firstInput, BufferedReader secondInput) {
+        left = firstInput;
+        right = secondInput;
+        if (left == null || right == null)
+            correct = false;
+        else
+            correct = true;
+    }
+
+    public void generateOutputTo(BufferedWriter bw) {
+        if (!correct)
+            return null;
+
+        while (left.)
+    }
+}
