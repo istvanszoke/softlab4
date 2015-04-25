f34d6287cd32584f745f83238d22b2177b08f5bd
Nyári Dávid Tamás <nyaridavid@gmail.com>
2015-04-25 12:26:25 +0200

    Further extention

--- src/main/graphics/GameGraphics.java
+++ src/main/graphics/GameGraphics.java
@@ -5,17 +5,26 @@ import javax.swing.JPanel;
 import java.awt.Graphics;
 import java.awt.image.BufferedImage;
 import java.awt.image.ImageObserver;
+import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 
+import agents.Agent;
+import buff.Buff;
+import buff.BuffListener;
+import field.Field;
 import game.Game;
 import graphics.handles.EmptyFieldCellDraw;
+import graphics.handles.RobotDraw;
 
-public class GameGraphics extends JPanel implements ImageObserver
+public class GameGraphics extends JPanel implements ImageObserver, BuffListener
 {
     private Game mainGame;
     private boolean refresh;
     private BufferedImage bufferedImage;
-
+    private Map<Field, DrawHandle> drawableFields;
+    private Map<Agent, DrawHandle> drawableRobots;
+    private Map<Buff, DrawHandle> drawableBuffs;
 
     private void setUp() {
 
@@ -56,5 +65,22 @@ public class GameGraphics extends JPanel implements ImageObserver
 
 
         refresh = false;
+    }
+
+    public Map<Field, DrawHandle> getDrawableFields() {
+        return Collections.unmodifiableMap(drawableFields);
+    }
+
+    public Map<Agent, DrawHandle> getDrawableRobots() {
+        return Collections.unmodifiableMap(drawableRobots);
+    }
+
+    public Map<Buff, DrawHandle> getDrawableBuffs () {
+        return Collections.unmodifiableMap(drawableBuffs);
+    }
+
+    @Override
+    public void onRemove(Buff buff) {
+        drawableBuffs.remove(buff);
     }
 }
