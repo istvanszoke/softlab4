6ee7ecff2ae7decc5967dc28e9531f7bd8d4579e
Gustorn <gustorn@gmail.com>
2015-05-11 00:56:48 +0200

    Win fix

--- build.bat
+++ build.bat
@@ -2,10 +2,20 @@
 
 if not exist %CD%\build mkdir %CD%\build
 
-javac -sourcepath %CD%\src\main -d build src\main\*.java
+javac -encoding utf8 -sourcepath %CD%\src\main -d build src\main\*.java
  
 if not "%1" == "run" goto norun
- 
+
 java -classpath %CD%\build Main
  
 :norun 
+
+if not "%1" == "jar" goto nojar
+
+if exist %CD%\softlab4.jar del %CD%\softlab4.jar
+cd build\
+jar cfe ..\softlab4.jar Main *
+cd ..
+
+:nojar
+
